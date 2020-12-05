ba6320b8441271548b7fc65e224f1e41bb4a2262
byfranfx <71705230+byfranfx@users.noreply.github.com>
2020-12-05 17:26:55 +0100

    SPDVI_DI01 : Test Listview
    testin new form With List View for get the Objects. i want get the listbox.DoubleClick object for create SQL form UPDATE, DELETE, NEW model.

---
+++ DI01_FranGomez_01/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using Dapper;
+
+namespace DI01_FranGomez_01
+{
+    public partial class Form1 : Form
+    {
+        List<Model> models = new List<Model>();
+        public Form1()
+        {
+            InitializeComponent();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["AdventureWorks2016"].ConnectionString;
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                
+                string sql = $@"select Production.ProductModel.ProductModelID, Production.ProductModel.Name, Production.ProductDescription.Description FROM Production.Product INNER JOIN Production.ProductSubcategory ON Production.Product.ProductSubcategoryID = Production.ProductSubcategory.ProductSubcategoryID INNER JOIN Production.ProductCategory ON Production.ProductSubcategory.ProductCategoryID = Production.ProductCategory.ProductCategoryID INNER JOIN Production.ProductModel ON Production.Product.ProductModelID = Production.ProductModel.ProductModelID INNER JOIN Production.ProductModelProductDescriptionCulture ON Production.ProductModel.ProductModelID = Production.ProductModelProductDescriptionCulture.ProductModelID INNER JOIN Production.ProductDescription ON Production.ProductModelProductDescriptionCulture.ProductDescriptionID = Production.ProductDescription.ProductDescriptionID WHERE ProductModelProductDescriptionCulture.CultureID = 'en' AND Product.ProductModelID IS NOT NULL";
+                models = connection.Query<Model>(sql).ToList();
+                foreach (Model model in models)
+                {
+                    listView1.Items.Add(model.ToString());
+                }
+            }
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            MessageBox.Show(string.Format("SelectedItem:\n{0}", listView1.SelectedItems.ToString()));
+        }
+    }
+}
---
+++ DI01_FranGomez_01/Model.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DI01_FranGomez_01
+{
+    class Model
+    {
+        public int ProductModelID { get; set; }
+        public string Name { get; set; }
+        public string Description  { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Description}";
+        }
+    }
+}
